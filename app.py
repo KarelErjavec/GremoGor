fc8a75c989b3e955e427ab08541266db84848a0c
KarelErjavec <karel.sast@gmail.com>
2025-03-28 10:33:05 +0100

    homepage - nedokončano

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def domov():
-    return render_template('index.html', podatek="vrednost")
+    return render_template('index.html', prijava=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
